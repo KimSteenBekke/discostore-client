dca87015cfc6f029442c0d2304e3278139013207
Kim Steen Bekke <65952389+KimSteenBekke@users.noreply.github.com>
2022-03-07 10:22:49 +0100

    album-component (#4)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ function Navbar() {
         <button>Home</button>
       </Link>
 
-      <Link to="/projects">
-        <button>Projects</button>
+      <Link to="/albums">
+        <button>Albums</button>
       </Link>
     </nav>
   );

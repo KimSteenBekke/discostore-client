054e7ce6fb41813383571d45940b1ffb1af3c261
Peter Ravnsbæk <dkpetrav@lego.com>
2022-03-09 10:17:12 +0100

    buy button as IsLoggedIn

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,27 +1,26 @@
-import { Link } from "react-router-dom";
-import AddOrder from "./AddOrder";
-import { useContext } from 'react';
-import { AuthContext } from "../context/auth.context";
-
-
+import { Link } from 'react-router-dom'
+import AddOrder from './AddOrder'
+import { useContext } from 'react'
+import { AuthContext } from '../context/auth.context'
 
 function AlbumCard({ title, artist, genre, price, imageURL, _id }) {
-const {user, isLoggedIn} = useContext(AuthContext);
-
+  const { user, isLoggedIn } = useContext(AuthContext)
 
   return (
     <div className="AlbumCard">
+      <h3>{title}</h3>
+      <h3>{artist}</h3>
+      <h3>{genre}</h3>
+      <h3>{price}</h3>
+      <img src={imageURL} alt="album cover" />
 
-      <Link to={`/albums/${_id}`}>
-        <h3>{title}</h3>
-        <h3>{artist}</h3>
-        <h3>{genre}</h3>
-        <h3>{price}</h3>
-        <img src={imageURL} alt="album cover" />
-        <button type="submit">Add Order</button>
-      </Link>
+      {isLoggedIn && (
+        <Link to={`/albums/${_id}`}>
+          <button type="submit">Add Order</button>
+        </Link>
+      )}
     </div>
-  );
+  )
 }
 
-export default AlbumCard;
+export default AlbumCard

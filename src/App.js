99e8aea80ecfd4345f74dcc84f51cdaa33838628
Peter Ravnsbæk <dkpetrav@lego.com>
2022-03-10 21:09:28 +0100

    MINIMUM viable product!

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
           />
 
         <Route
-          path="/order/:orderId"
+          path="/order/x/:orderId"
           element={
             <IsPrivate>
               <EditOrderPage />{" "}

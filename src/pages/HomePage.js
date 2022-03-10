651eeb85dbbc9ec1450ff54e3bb9f2660607626c
Peter Ravnsbæk <dkpetrav@lego.com>
2022-03-10 18:41:26 +0100

    HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,12 @@
 function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Welcome to DiscoStore Record Database</h1>
+      <p>if you don't find what you're looking for - let's append it 👍</p>
+
+       <img src="..//public/IMG_0938.jpeg" alt="Pick-up CORAL 555-SH image" /> 
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
